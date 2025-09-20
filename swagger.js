f3e4e86629e951b8598b93af4c607d2e8a37cbb3
mrAmjadChel <amjadchelaeh.8@gmail.com>
2025-09-21 02:26:59 +0700

    add cors

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       { url: 'http://localhost:3000', description: 'Local dev' },
-      { url: 'https://api.myapp.com', description: 'Production' }
+      { url: 'https://stations-api-i4uo.onrender.com/docs', description: 'Production' }
     ],
     components: {
       securitySchemes: {
